8d0b219070db8ce657c833156d1c7430e8b4d6c3
MaryBhavana <bhavanagollapalli123@gmail.com>
2022-07-10 16:08:44 +0530

    day4

---
+++ src/Components/Day4/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Memo from './Memo';
+import Reducer from './Reducer';
+import Ref from './Ref';
+
+function App() {
+    return (
+      <div className="App">
+        <Memo/>
+        <Reducer/>
+        <Ref/>
+      </div>
+    );
+  }
+  
+  export default App;
